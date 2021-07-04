10044421af1297f7d87d1c69234cc783849ac048
ianfelix <ianfelix030@gmail.com>
2021-07-04 13:55:46 -0300

    feat: create landing page

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a376a065936140b3f9c6ed896691e639'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3b60c855-6d7e-41a1-b9a4-ed02cc9e7dc4'
+};
